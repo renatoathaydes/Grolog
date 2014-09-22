b76b099aa6204b725128befdbdfcb74b3ba47296
Renato Athaydes <renatoathaydes@hotmail.com>
2014-09-22 23:14:51 +0200

    Improved GrologRunner with command syntax and usage info

--- test/com/athaydes/grolog/GrologRunner.groovy
+++ test/com/athaydes/grolog/GrologRunner.groovy
@@ -3,11 +3,25 @@ package com.athaydes.grolog
 import com.athaydes.grolog.internal.parser.GrologParser
 import org.apache.tools.ant.filters.StringInputStream
 
+import java.util.regex.Matcher
+
 class GrologRunner {
 
+    static final USAGE = """|************ Grolog ************
+                            |Type '!' to add predicates
+                            |     '?' to enter queries
+                            |
+                            |Commands:
+                            |  * '!q' or '!quit'     = quit Grolog
+                            |  * '!h' or '!help'     = print this usage help
+                            |  * '!c' or '!clear'    = clear all predicates
+                            |""".stripMargin()
+
     static void main( String[] args ) {
+        println USAGE
+
         def grolog = new Grolog()
-        def grologParser = new GrologParser()
+        def parser = new GrologParser()
         def queryMode = false
         print( queryMode ? '?- ' : '!- ' )
         System.in.eachLine { input ->
@@ -15,18 +29,22 @@ class GrologRunner {
                 switch ( input ) {
                     case '?': queryMode = true
                         break
-                    case '!': queryMode = false
+                    case ~/!(.*)/:
+                        def cmd = matcher[ 0 ][ 1 ]
+                        if ( cmd ) {
+                            runCmd cmd, grolog
+                        } else {
+                            queryMode = false
+                        }
                         break
-                    case 'q':
-                        println "Bye!"
-                        System.exit( 0 )
                     default:
                         if ( queryMode ) {
-                            def parts = input.split()
-                            if ( !parts ) println 'You must provide a command'
-                            else println grolog.query( parts.first(), *parts.tail() )
+                            def parts = input.split( /[(\,;\.\s)]/ ).collect { it?.trim() }.findAll { it }
+                            println parts ?
+                                    grolog.query( parts.first(), *parts.tail() ) :
+                                    "You must enter a query or command. Enter '!help' for usage."
                         } else {
-                            grolog.merge( grologParser.from( new StringInputStream( input ) ) )
+                            grolog.merge( parser.from( new StringInputStream( input ) ) )
                         }
 
                 }
@@ -36,6 +54,28 @@ class GrologRunner {
             print( queryMode ? '?- ' : '!- ' )
         }
 
+    }
+
+    static void runCmd( String cmd, Grolog grolog ) {
+        switch ( cmd ) {
+            case 'q':
+            case 'quit':
+                println "Bye!"
+                System.exit( 0 )
+                break // avoid warnings
+            case 'h':
+            case 'help':
+                println USAGE
+                break
+            case 'c':
+            case 'clear':
+                grolog.clear()
+
+        }
+    }
+
+    static Matcher getMatcher() {
+        Matcher.lastMatcher
     }
 
 }
